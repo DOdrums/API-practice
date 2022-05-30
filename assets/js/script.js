be291d00fc8b090784262bf733e1c4cc5bd03250
DOdrums <dirkornee@hotmail.com>
2022-05-30 10:49:03 +0000

    add js script to fetch data from api and display key expiry date in modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,28 @@
-aNYHpGzZKAB7HD27Fok_gqh57Tw
+const API_KEY = "aNYHpGzZKAB7HD27Fok_gqh57Tw";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"))
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
+function displayStatus(data) {
+    document.getElementById("resultsModalTitle").innerText = "API Key Status";
+    document.getElementById("results-content").innerHTML = `
+    Your key is valid until 
+    <div>${data.expiry}</div>
+    `;
+    resultsModal.show();
+}
